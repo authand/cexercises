cd2f8897261b6a27285a1a0d7eb0339c03f908de
shinji <sokarihinji@gmail.com>
2025-06-22 22:27:11 +0300

    changed header

--- 1.2_variables_and_arithmetic_expressions/temperature_fahr.c
+++ 1.2_variables_and_arithmetic_expressions/temperature_fahr.c
@@ -9,7 +9,7 @@ int main() {
     step = 20;
 
     celsius = lower;
-    printf("---- Celsius to Fahrenheit ----\n");
+    printf("Celsius\tFahrenheit\n");
     while (celsius <= upper) {
         fahr = (celsius / (5.0/9.0)) + 32.0;
         printf("%3.0f\t%6.1f\n", celsius, fahr);
