90968efe47ceb54847adb3beb2f5902dadf821b1
sokari <sokarihinji@gmail.com>
2025-07-04 15:33:15 +0000

    finish ex. 1-18

--- 1.9_character_arrays/exercise_1-18.c
+++ 1.9_character_arrays/exercise_1-18.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define MAXLINE 1024
 
-int getline(char line[], int maxline);
+int get_line(char line[], int maxline);
 void rtrim(char line[]);
 
 int main() {
     char line[MAXLINE];
 
-    while (getline(line, MAXLINE) > 0) {
+    while (get_line(line, MAXLINE) > 0) {
         rtrim(line);
 
         if (line[0] != '\0') {
@@ -18,7 +18,7 @@ int main() {
     return 0;
 }
 
-int getline(char s[], int lim) {
+int get_line(char s[], int lim) {
     int c, i;
 
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
@@ -30,5 +30,15 @@ int getline(char s[], int lim) {
 }
 
 void rtrim(char s[]) {
+    int i;
+
+    for (i = 0; s[i] != '\0'; ++i) {
+        ;
+    }
+    --i;
     
+    while (i >= 0 && s[i] == ' ' || s[i] == '\t')  {
+        --i;
+    }
+    s[i + 1] = '\0';
 }
