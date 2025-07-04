0716b2a1f4a21471a0a4800a4c43e001eeb5a216
sokari <sokarihinji@gmail.com>
2025-07-04 13:53:03 +0000

    start 1-18

--- 1.9_character_arrays/exercise_1-18.c
+++ 1.9_character_arrays/exercise_1-18.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#define MAXLINE 1024
+
+int getline(char line[], int maxline);
+void rtrim(char line[]);
 
 int main() {
+    char line[MAXLINE];
 
+    while (getline(line, MAXLINE) > 0) {
+        rtrim(line);
+
+        if (line[0] != '\0') {
+            printf("%s\n", line);
+        }
+    }
+
+    return 0;
+}
+
+int getline(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+    s[i] = '\0';
+
+    return i;
+}
+
+void rtrim(char s[]) {
+    
 }
