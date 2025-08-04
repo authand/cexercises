ceb1eab101a928532e60dc92c1ae0712a9da50a1
sokari <sokarihinji@gmail.com>
2025-08-04 16:26:23 +0000

    clean up code

--- 1.10_external_variables_and_scope/exercise_1-20.c
+++ 1.10_external_variables_and_scope/exercise_1-20.c
@@ -7,22 +7,22 @@ int main() {
 
     while ((c = getchar()) != EOF) {
         if (c == '\t') {
+
             int spaces_to_add;
             spaces_to_add = TABSTOP - (column % TABSTOP);
             for (int i = spaces_to_add; i > 0; i--) {
                 putchar(' ');
-            }
 
             column = column + spaces_to_add;
-
+            }
         } else if (c == '\n') {
+
             putchar(c);
             column = 0;
-
         } else {
+
             putchar(c);
             column++;
-
         }
     }
 
